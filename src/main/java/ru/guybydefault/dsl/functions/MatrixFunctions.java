9afd5f4b75158b5c71740bff2473b83ac6e3bbd3
Dana Zhetesova <zetesovadana@gmail.com>
2020-01-11 16:38:39 +0300

    матрицы не удались

--- src/main/java/ru/guybydefault/dsl/functions/MatrixFunctions.java
+++ src/main/java/ru/guybydefault/dsl/functions/MatrixFunctions.java
@@ -1,5 +1,14 @@
 package ru.guybydefault.dsl.functions;
 
-public class MatrixFunctions {
+import ru.guybydefault.domain.Expression;
+import ru.guybydefault.domain.StringSymbol;
+import ru.guybydefault.dsl.library.Functions;
 
+public class MatrixFunctions {
+    public static final StringSymbol Matrix = new StringSymbol("Matrix");
+    public static final StringSymbol MatrixPlus = new StringSymbol("MatrixPlus");
+
+    public static Expression MatrixPlusImplementation() {
+        return new Expression(Functions.Fun, new Expression(ArithmeticFunctions.ListPlus, ));
+    }
 }
