833358105e4dff1a3aeaba6de8f1fbd982488070
Dana Zhetesova <zetesovadana@gmail.com>
2020-03-12 14:57:57 +0300

    new test matrix mult

--- src/test/java/src/main/java/matrixOperations/MatrixMulOperationsTest.java
+++ src/test/java/src/main/java/matrixOperations/MatrixMulOperationsTest.java
@@ -34,4 +34,28 @@ public class MatrixMulOperationsTest {
                 ))
         );
     }
+
+    @org.junit.Test
+    public void testMatrixMulMatrix_2() {
+        evaluateAndAssert(
+                new Expression(
+                        MatrixFunctions.MatrixMult, Arrays.asList(
+                        new Expression(ListFunctions.List,
+                                new Expression(ListFunctions.List, new Constant(0.24), new Constant(-0.68), new Constant(0.4)),
+                                new Expression(ListFunctions.List, new Constant(-0.28), new Constant(-0.04), new Constant(0.2)),
+                                new Expression(ListFunctions.List, new Constant(-0.2), new Constant(0.4), new Constant(-0.0))
+                        ),
+                        new Expression(ListFunctions.List,
+                                new Expression(ListFunctions.List, new Constant(1)),
+                                new Expression(ListFunctions.List, new Constant(3)),
+                                new Expression(ListFunctions.List, new Constant(2))
+                        ))
+                ),
+                new Expression(ListFunctions.List,
+                        new Expression(ListFunctions.List, new Constant(-1)),
+                        new Expression(ListFunctions.List, new Constant(0)),
+                        new Expression(ListFunctions.List, new Constant(1.0000000000000002))
+                )
+        );
+    }
 }
